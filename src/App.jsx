6b5296440d69de2b78ecac3b8895cdce7000624a
Yousefkii <youssefhegaze908@gmail.com>
2025-02-24 00:58:24 +0100

    uploading the material and strucutre

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-amber-50">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, magnam, cum aut dolorum numquam maxime ad, aliquam voluptatem facilis rem facere? Quo iure excepturi porro numquam quam, ea d
+        
+      </p>
+    </>
+  )
+}
+
+export default App
