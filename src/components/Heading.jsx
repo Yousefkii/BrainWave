1ded18ae71ecc0d3a68a663e61a66849391bf10a
Yousefkii <youssefhegaze908@gmail.com>
2025-02-28 17:53:38 +0100

    Creating the services section

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Heading = ({className,title}) => {
+const Heading = ({className,title,text}) => {
   return (
-    <div className={`${className} text-4xl p-3 max-w-[50rem] mx-auto mb-12 lg:mb-20  lg:text-8xl `}>
+    <div className={`${className} text-4xl p-3 max-w-[50rem] mx-auto mb-12 lg:mb-20  lg:text-8xl ml-5 lg:w-auto`}>
         {title && <h2 className='h2'> {title} </h2>}
+        {text && <p className='body-2 mt-7 text-2xl text-white/40'>{text}</p>}
     </div>
   )
 }
